aa6d7ac52d9bf5ab02d6dee7b9f819b421d36763
Junsung Kim <wnstjd123@ajou.ac.kr>
2024-05-17 23:42:25 +0900

    test commit

---
+++ mission/mission1.c
@@ -0,0 +1,15 @@
+#include <sys.stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define IN 0
+#define LOW 0
+#define OUT 1
+#define HIGH 1
+#define BTN_PIN 20
+#define BTN_PIN2 21
+#define LED_PIN 17
+#define VALUE_MAX 40
